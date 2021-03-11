8bbb776ee7d77fc478146c937507ac82e9ac02a7
jet-c-21 <67193495+jet-c-21@users.noreply.github.com>
2021-03-11 18:28:28 +0800

    Update app.js

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,5 +1,17 @@
-var a = 'xd';
+// if (a === undefined) {
+//     console.log('a is undefined!');
+// } else {
+//     console.log('a is defined!');
+// }
 
 function b() {
-
+    console.log('called b!');
 }
+
+b();
+
+console.log(a);
+
+var a = 'xd';
+
+console.log(a);
