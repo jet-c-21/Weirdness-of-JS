e3183927afb7c930c3bedc5abc61fbc5b76561e3
jet-c-21 <67193495+jet-c-21@users.noreply.github.com>
2021-03-13 12:48:11 +0800

    UNKONW

--- B9_VariableEnvironments/Starter/app.js
+++ B9_VariableEnvironments/Starter/app.js
@@ -1,14 +1,16 @@
-function b() {
-    var myVar;
-    console.log(myVar);
-}
+
 
 function a() {
-    var myVar = 2;
-    console.log(myVar);
+    function b() {
+        // var myVar;
+        console.log(myVar);
+    }
+
+    // var myVar = 2;
+    // console.log(myVar);
     b();
 }
 
 var myVar = 1;
-console.log(myVar);
 a();
+
